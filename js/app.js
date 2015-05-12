13f7c87a213e05ba6f0fa71df0e8b4996a9f8722
Ken Howard <ken@simplicatedweb.com>
2015-05-12 15:14:53 -0700

    Added Google Analytics Code.

--- js/app.js
+++ js/app.js
@@ -5,3 +5,15 @@ $(document).foundation();
 $('a').filter(function() {
 	return this.hostname && this.hostname !== location.hostname;
 }).attr("target","_blank");
+
+if (location.host === 'kenhowardpdx.com') {
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-6386829-3']);
+	_gaq.push(['_trackPageview']);
+	
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+}
